ff44083b58a948b455f76563f617578355816ad1
Ahmed Shahid <Ahmed_shahid50@yahoo.com>
2020-11-25 01:13:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> hello world ahmed shahid its my first app..!</h1>
+      <h1> hello world ahmed shahid its my first!</h1>
     </div>
   );
 }
